6a1884ddea2d5c4339277172dbd1d43c80b3453f
Futuret3chdev <jason.c@futuret3ch.com.au>
2026-06-19 16:25:21 +1000

    Rebrand from Toon Blast to MTE POP

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 PORT=${1:-8765}
-echo "🎮 Toon Blast running at http://localhost:$PORT"
+echo "🎮 MTE POP running at http://localhost:$PORT"
 echo "   Press Ctrl+C to stop"
 ruby -run -e httpd . -p "$PORT"
